a1a8654ed930ae4e52c8884187bef6f89f9f7590
arsl11 <arifullin1100@mail.ru>
2023-07-11 14:53:37 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'
     >
-      <div className={'${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto px-6 flex sm:flex-row sm:justify-between flex-col'}>
+      <div className={'${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto px-6 flex xs:flex-row xs:justify-between flex-col'}>
         <div className={`flex flex-row items-start gap-5`}>
           <div className='flex flex-col
           justify-center items-center mt-5'>
@@ -28,7 +28,7 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className='sm:h-auto h-[350px]'>
+        <div className='xs:h-auto h-[350px]'>
           <AvatarCanvas />
         </div>
       </div>
